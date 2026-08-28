603799dad821437d830f4429e31ab53c6f989e03
luka1233-cpu <luka1233@gmail.com>
2026-08-28 14:32:17 +0200

    Add files via upload

---
+++ iwt-sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'iwt-cache-v2';
+const ASSETS = [
+  './iwt-speed-tracker.html',
+  './iwt-manifest.json',
+  './iwt-icon-192.png',
+  './iwt-icon-512.png'
+];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE_NAME).then(function(c){ return c.addAll(ASSETS); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(cached){
+      return cached || fetch(e.request).catch(function(){ return cached; });
+    })
+  );
+});
